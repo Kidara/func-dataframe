b23eca02f1b5355c1c7ba713e62fcc72b4847df7
Davide D'Ascenzo <davide.dascenzo.work@gmail.com>
2023-05-04 13:47:39 +0200

    Update func_dataframe.py

--- src/func_dataframe.py
+++ src/func_dataframe.py
@@ -1,98 +1,85 @@
 from pandas import DataFrame
+from pandas.api.extensions import register_dataframe_accessor
 from inspect import signature
+from functools import reduce
 
-def func_df(ignore_args = {}, **kwargs):
+def fdf_func(ignore_args = {}, **kwargs):
   def decorator(func):
     func_kwargs = {}
     for p in signature(func).parameters.values():
-      #if p.kind == inspect.Parameter.POSITIONAL_ONLY or p.kind == inspect.Parameter.VAR_POSITIONAL:
-        #raise ValueError(f"[{func.__name__}] Only keyword arguments are allowed")
       if p.name in ignore_args:
         continue
       func_kwargs[p.name] = p.name
       
     if not kwargs.keys() <= func_kwargs.keys():
-      raise ValueError(f"[{func.__name__}] Invalid keyword arguments in func_df decorator")
+      raise ValueError(f"[{func.__name__}] Invalid keyword arguments in fdf_func decorator")
     
     func_kwargs.update(kwargs)
     func.func_df_dict = func_kwargs
     return func
   return decorator
 
+  
+def _dask_map(func):
+  import func_dataframe
+  def _inner(partition, func=func):
+    return partition.apply(func, axis=1)
+  return _inner
 
-class FuncDataFrame(DataFrame):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.funcs = {}
-    
-  def decorate_func(self, func, column):
+@register_dataframe_accessor("fdf")
+class FuncDataFrame:
+  def __init__(self, pandas_obj):
+    self.obj = pandas_obj
+  
+  def decorate_func(self, func, columns):
+    for c in func.func_df_dict:
+      if func.func_df_dict[c] not in self.obj:
+        raise ValueError(f"[{func.__name__}] Column '{func.func_df_dict[c]}' not found in the dataframe")
     def wrapper(row):
-      #print(f"Row: {row}")
-      if row[column] is not None:
+      if row[columns].apply(lambda x: x != None).all():
         return row
+      
       for c in func.func_df_dict:
-        if c in self.funcs:
-          row = self.funcs[c](row)
+        if c in self.cf_dict and row[c] is None:
+          row = self.cf_dict[c](row)
       
       func_kwargs = {}
-      #print(f"func.func_df_dict: {func.func_df_dict}")
       for c in func.func_df_dict:
-        if func.func_df_dict[c] not in row:
-          raise ValueError(f"[{func.__name__}] Column '{func.func_df_dict[c]}' not found in the dataframe")
         func_kwargs[c] = row[func.func_df_dict[c]]
-      #print(f"row[column]: {row[column]}")
-      #print(f"func_kwargs: {func_kwargs}")
-      #print(f"func(func_kwargs): {func(**func_kwargs)}")
-      row[column] = func(**func_kwargs)
-      #print(f"row: {row}")
+      row[columns] = func(**func_kwargs)
       return row
     return wrapper
-      
-  def add_cf(self, cf_dict = None, column = None, func = None):
-    if (not column or not func) and not cf_dict:
-      #print(not column)
-      #print(not func)
-      raise ValueError("Either column and func or cf_dict must be provided")
-    if (column or func) and cf_dict:
-      raise ValueError("Either column and func or cf_dict must be provided, not both")
     
-    if column and func:
-      cf_dict = {column: func}
-    for column in cf_dict:
-      if not hasattr(cf_dict[column], 'func_df_dict'):
-        cf_dict[column] = func_df()(cf_dict[column])
-        #raise ValueError(f"Function {cf_dict[column].__name__} is not decorated")
-    for column in cf_dict:
-      cf_dict[column] = self.decorate_func(cf_dict[column], column)
-    self.funcs.update(cf_dict)
-
-    for column in cf_dict:
-      self[column] = None
+  def compute(self, cf_dict, parallel=False, n_dask_partitions=None, shuffle=True, scheduler='threads'):
+    cf_dict = cf_dict.copy()
     
-  def del_cf(self, column):
-    if column in self.funcs:
-      del self.funcs[column]
-      self.drop(column, axis=1, inplace=True)
+    cc = list(cf_dict.keys())
+    for c in cc:
+      if not isinstance(c, tuple):
+        cf_dict[(c,)] = cf_dict.pop(c)
+    
+    for c in cf_dict:
+      if not hasattr(cf_dict[c], 'func_df_dict'):
+        cf_dict[c] = fdf_func()(cf_dict[c])
+      cf_dict[c] = self.decorate_func(cf_dict[c], list(c))
+    
+    self.cf_dict = cf_dict
+        
+    cc = reduce(lambda c1, c2: c1 + c2, cf_dict.keys())
+    for c in cc:
+      self.obj[c] = None
       
-  def rename_cf(self, old_column, new_column):
-    if old_column in self.funcs:
-      self.funcs[new_column] = self.funcs[old_column]
-      del self.funcs[old_column]
-      self.rename(columns={old_column: new_column}, inplace=True)
-      
-  def update_cf_df(self, column = None, parallel = False):
-    if column:
-      if column not in self.funcs:
-        raise ValueError(f"Column {column} not found in funcs")
+    if shuffle:
+      self.obj = self.obj.sample(frac=1).reset_index(drop=True)
+    for func in cf_dict.values():
       if parallel:
-        import swifter
-        self[self.columns] = self.swifter.apply(self.funcs[column], axis = 1)
+        from dask import dataframe as dd
+        from dask.diagnostics import ProgressBar
+        with ProgressBar():
+          self.obj[:] = dd.from_pandas(self.obj, npartitions=n_dask_partitions).map_partitions(lambda p, f=func: p.apply(f, axis=1)).compute(scheduler=scheduler)
       else:
-        self[self.columns] = self.apply(self.funcs[column], axis = 1)	
-    else:
-      for column, func in self.funcs.items():
-        if parallel:
-          import swifter
-          self[self.columns] = self.swifter.apply(func, axis = 1)
-        else:
-          self[self.columns] = self.apply(func, axis = 1)
+        from tqdm import tqdm
+        tqdm.pandas()
+        self.obj[:] = self.obj.progress_apply(func, axis = 1)
+        
+    return self.obj
